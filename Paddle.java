df957984a96c90e121ecb1783a6dbb41afbbd72a
Shaheer <shaheerimran731@gmail.com>
2019-11-04 13:07:38 -0800

    Update Paddle.java

--- Paddle.java
+++ Paddle.java
@@ -3,29 +3,56 @@ import java.awt.Graphics;
 
 
 public class Paddle {
+	//Coordinates
 	private int x;
 	private int y;
-	
+
+	//Dimensions
 	private int height;
 	private int width;
 	
+	//Vertical velocity
 	private int velocity;
 	
+	//Score
 	private int score;
 	
+	//Color
 	private Color c;
 	
-	public Paddle(int x_param, int y_param, int w, int h, Color hue){
+	public Color getC() {
+		return c;
+	}
+
+	public void setC(Color c) {
+		this.c = c;
+	}
+	
+	public Paddle(){
+		x = 10;
+		y = 10;
+		height = 140;
+		width = 20;
+		c = Color.white;
+		velocity = 0;
+	}
+	
+	public Paddle(int x_param, int y_param, int w, int h, Color hue, int v){
 		x = x_param;
 		y = y_param;
 		height = h;
 		width = w;
 		c = hue;
+		velocity = v;
 	}
 	
 	public void paint(Graphics g){
 		g.setColor(c);
 		g.fillRect(x, y, width, height);
+	}
+	
+	public void move(){
+		y += velocity;
 	}
 	
 	public int getX(){
